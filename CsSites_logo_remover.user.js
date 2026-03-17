14c7c39314f9a04a4e933287c16fcd568bacc09c
Osnova Osnov <plachnaru@gmail.com>
2026-03-17 15:30:09 +0100

    добавлен skinhouse

--- CsSites_logo_remover.user.js
+++ CsSites_logo_remover.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         CsSites_logo_remover
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Replace runcase.gg and runcase.net logos
+// @version      1.2
+// @description  Replace runcase.gg, runcase.net and skinhouse.gg logos
 // @match        https://runcase.gg/*
 // @match        https://runcase.net/*
+// @match        https://skinhouse.gg/*
 // @icon         https://runcase.gg/img/runcase-logo.svg
 // @updateURL    https://raw.githubusercontent.com/Osnova-Osnov/CsSites_logo_remover/main/CsSites_logo_remover.user.js
 // @downloadURL  https://raw.githubusercontent.com/Osnova-Osnov/CsSites_logo_remover/main/CsSites_logo_remover.user.js
@@ -14,35 +15,55 @@
 (function() {
     'use strict';
 
-    const newLogo = "https://raw.githubusercontent.com/Osnova-Osnov/CsSites_logo_remover/refs/heads/main/runcase-logo.svg";
+    // Новые логотипы
+    const newLogoRuncase = "https://raw.githubusercontent.com/Osnova-Osnov/CsSites_logo_remover/refs/heads/main/runcase-logo.svg";
+    const newLogoSkinhouse = "https://raw.githubusercontent.com/Osnova-Osnov/CsSites_logo_remover/refs/heads/main/skinhouse.svg";
 
     // Логотипы, которые нужно заменить
-    const originalLogos = [
-        "runcase-logo.svg",                 // общий случай
+    const originalLogosRuncase = [
+        "runcase-logo.svg",
         "https://runcase.gg/img/runcase-logo.svg",
         "https://runcase.net/img/runcase-logo.svg"
     ];
 
+    const originalLogosSkinhouse = [
+        "logo-menu.svg",
+        "https://skinhouse.gg/images/panel-header/logo-menu.svg"
+    ];
+
     function replaceLogo() {
-        // Меняем <img>
         const imgs = document.querySelectorAll("img");
+        const elements = document.querySelectorAll('*');
+
+        // Runcase
         imgs.forEach(img => {
-            if (originalLogos.some(url => img.src.includes(url))) {
-                img.src = newLogo;
+            if (originalLogosRuncase.some(url => img.src.includes(url))) {
+                img.src = newLogoRuncase;
             }
         });
 
-        // Меняем background-image
-        const elements = document.querySelectorAll('*');
         elements.forEach(el => {
             const bg = getComputedStyle(el).backgroundImage;
-            if (originalLogos.some(url => bg.includes(url))) {
-                el.style.backgroundImage = `url("${newLogo}")`;
+            if (originalLogosRuncase.some(url => bg.includes(url))) {
+                el.style.backgroundImage = `url("${newLogoRuncase}")`;
+            }
+        });
+
+        // Skinhouse
+        imgs.forEach(img => {
+            if (originalLogosSkinhouse.some(url => img.src.includes(url))) {
+                img.src = newLogoSkinhouse;
+            }
+        });
+
+        elements.forEach(el => {
+            const bg = getComputedStyle(el).backgroundImage;
+            if (originalLogosSkinhouse.some(url => bg.includes(url))) {
+                el.style.backgroundImage = `url("${newLogoSkinhouse}")`;
             }
         });
     }
 
-    // Запуск сразу и при изменениях DOM
     replaceLogo();
     const observer = new MutationObserver(replaceLogo);
     observer.observe(document.body, { childList: true, subtree: true });
